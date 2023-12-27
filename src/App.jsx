6e0a53b84c32adab076c882a783dd2aab8c54272
dev-abdullah018 <144674090+dev-abdullah018@users.noreply.github.com>
2023-12-28 04:11:15 +0600

    update-navbar-responsive

--- src/App.jsx
+++ src/App.jsx
@@ -3,28 +3,47 @@ import Image from './components/layout/Image';
 import Flex from './components/layout/Flex';
 import List from './components/layout/List';
 import ListItem from './components/layout/ListItem';
+import { FaBars } from "react-icons/fa6";
+import { useEffect, useState } from 'react';
 function App() {
+  let [show,setShow] = useState(true);
+
+  useEffect(()=>{
+    function scrollWidth(){
+      if(window.innerWidth < 1024){
+        setShow(false)
+      }else{
+        setShow(true)
+      }
+    }
+    window.addEventListener("resize", scrollWidth)
+  },[])
   return (
-    <div>
+    <>
       <nav>
         <div className="max-w-container mx-auto p-2.5">
-          <Flex className='flex'>
-            <div className="w-3/12">
+          <Flex className='lg:flex'>
+            <div className="lg:w-3/12">
               <Image src={logo} alt='orebi' />
             </div>
-            <div className="w-9/12">
-              <List className='flex justify-end gap-x-10'>
-                <ListItem className='font-dm text-sm font-normal hover:font-bold' itemname="Home" /> 
-                <ListItem className='font-dm text-sm font-normal hover:font-bold' itemname="Shop"/>
-                <ListItem className='font-dm text-sm font-normal hover:font-bold' itemname="About"/>
-                <ListItem className='font-dm text-sm font-normal hover:font-bold' itemname="Contacts"/>
-                <ListItem className='font-dm text-sm font-normal hover:font-bold' itemname="Journal"/>
+            <div className="lg:w-9/12 w-full">
+            <FaBars onClick={()=> setShow(!show)} className='block lg:hidden ml-auto absolute top-2.5 right-2.5'/>
+            {
+              show && (
+                <List className='lg:flex lg:justify-end lg:gap-x-10 mt-5 lg:mt-0'>
+                <ListItem className='font-dm text-sm font-normal hover:font-bold my-2.5 lg:my-0' itemname="Home" /> 
+                <ListItem className='font-dm text-sm font-normal hover:font-bold my-2.5 lg:my-0' itemname="Shop"/>
+                <ListItem className='font-dm text-sm font-normal hover:font-bold my-2.5 lg:my-0' itemname="About"/>
+                <ListItem className='font-dm text-sm font-normal hover:font-bold my-2.5 lg:my-0' itemname="Contacts"/>
+                <ListItem className='font-dm text-sm font-normal hover:font-bold my-2.5 lg:my-0' itemname="Journal"/>
               </List>
+              )
+            }
             </div>
           </Flex>
         </div>
       </nav>
-    </div>
+    </>
   );
 }
 
