1fc860921a0cda02c3be1e3ccc93ddf52b23c8b4
dev-abdullah018 <144674090+dev-abdullah018@users.noreply.github.com>
2023-12-29 00:39:05 +0600

    update-dropdown-design

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -24,13 +24,16 @@ const Header = () => {
       <Container>
       <Flex className="justify-between">
         <div >
-        <Dropdown dropref={ref} titleicon={<HiBars3BottomLeft />} titletext="Shop by Category" className="yourListItemClassName">
+        <Dropdown dropref={ref} titleicon={<HiBars3BottomLeft />} titletext="Shop by Category" className=" relative">
             {
             show && (
-            <List className="absolute">
-              <ListItem itemname="Accesories" className="yourListItemClassName"/>
-              <ListItem itemname="Accesories" className="yourListItemClassName"/>
-              <ListItem itemname="Accesories" className="yourListItemClassName"/>
+            <List className="absolute top-8 w-[263px] bg-primary text-[#767676]">
+              <ListItem itemname="Accesories" className="py-4 px-5 hover:px-6 border border-solid border-[#2D2D2D] hover:text-white ease-in duration-300 "/>
+              <ListItem itemname="Furniture" className="py-4 px-5 hover:px-6 border border-solid border-[#2D2D2D] hover:text-white ease-in duration-300"/>
+              <ListItem itemname="Electronics" className="py-4 px-5 hover:px-6 border border-solid border-[#2D2D2D] hover:text-white ease-in duration-300"/>
+              <ListItem itemname="Clothes" className="py-4 px-5 hover:px-6 border border-solid border-[#2D2D2D] hover:text-white ease-in duration-300"/>
+              <ListItem itemname="Bags" className="py-4 px-5 hover:px-6 border border-solid border-[#2D2D2D] hover:text-white ease-in duration-300"/>
+              <ListItem itemname="Home appliances" className="py-4 px-5 hover:px-6 border border-solid border-[#2D2D2D] hover:text-white ease-in duration-300"/>
             </List>
               )
             }
--- src/components/layout/ListItem.jsx
+++ src/components/layout/ListItem.jsx
@@ -1,12 +1,14 @@
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
-function ListItem({itemname,className}) {
-  return <li className={className}>{itemname}</li>
+function ListItem({itemname,className,href}) {
+  return <li className={className}><Link to={href}>{itemname}</Link></li>
 }
 
 ListItem.propTypes={
  itemname: PropTypes.node.isRequired,
- className: PropTypes.string.isRequired
+ className: PropTypes.string.isRequired,
+ href: PropTypes.string.isRequired, 
 }
 
 export default ListItem;
