6dcb226849b956e5dc1ef4ac631278c6f7ce9ad4
dev-abdullah018 <144674090+dev-abdullah018@users.noreply.github.com>
2024-01-04 18:10:08 +0600

    update-banner-bottom-responsive

--- src/components/layout/AdditionalInfo.jsx
+++ src/components/layout/AdditionalInfo.jsx
@@ -8,20 +8,20 @@ import { SlReload } from "react-icons/sl";
 const AdditionalInfo = () => {
   return (
       <>
-      <div className='border border-solid border-[#f0f0f0] py-7 -mt-[6px]'>
+      <div className='border border-solid border-[#f0f0f0] py-2 sm:py-7 -mt-[6px]'>
       <Container>
         <Flex className="flex justify-between">
-          <Flex className="flex items-center gap-x-4">
-          <RiNumber2 className='text-2xl'/>
-          <p className='font-dm font-normal text-base text-[#6d6d6d]'>Two years warranty</p>
+          <Flex className="flex items-center gap-x-1 sm:gap-x-4">
+          <RiNumber2 className='text-[10px] sm:text-2xl'/>
+          <p className='font-dm font-normal text-[10px] sm:text-base text-[#6d6d6d]'>Two years warranty</p>
           </Flex>
-          <Flex className="flex items-center gap-x-4">
-          <FaTruck className='text-2xl'/>
-          <p className='font-dm font-normal text-base text-[#6d6d6d]'>Free shipping</p>
+          <Flex className="flex items-center gap-x-1 sm:gap-x-4">
+          <FaTruck className='text-[10px] sm:text-2xl'/>
+          <p className='font-dm font-normal text-[10px] sm:text-base text-[#6d6d6d]'>Free shipping</p>
           </Flex>
-          <Flex className="flex items-center gap-x-4">
-          <SlReload className='text-[18px] rotate-[120deg] stroke-[30px]'/>
-          <p className='font-dm font-normal text-base text-[#6d6d6d]'>Return policy in 30 days</p>
+          <Flex className="flex items-center gap-x-1 sm:gap-x-4">
+          <SlReload className='text-[10px] sm:text-[18px]  rotate-[120deg] stroke-[30px]'/>
+          <p className='font-dm font-normal text-[10px] sm:text-base text-[#6d6d6d]'>Return policy in 30 days</p>
           </Flex>
         </Flex>
       </Container>
