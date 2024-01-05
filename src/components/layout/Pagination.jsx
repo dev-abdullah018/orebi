d4604ecf25f5e85dea2ee94a8cffe41b5d40d46a
dev-abdullah018 <144674090+dev-abdullah018@users.noreply.github.com>
2024-01-05 16:26:48 +0600

    update-product-page-pagination

--- src/components/layout/Pagination.jsx
+++ src/components/layout/Pagination.jsx
@@ -1,70 +1,60 @@
-import React, { useEffect, useState } from 'react';
-import ReactPaginate from 'react-paginate';
-import Product from './Product';
-import P1 from "../../assets/p1.png"
-// Example items, to simulate fetching from another resources.
-const items = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14,15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28,29,30,31,32,33,34,35,36];
+import React, { useEffect, useState } from "react";
+import ReactPaginate from "react-paginate";
+import Product from "./Product";
+import P1 from "../../assets/p1.png";
+
+const items = [...Array(120).keys()];
 
 function Items({ currentItems }) {
   return (
     <>
-      {currentItems &&
-        currentItems.map((item) => (
-          <div className='w-[32%]'>
-          <Product src={P1} badge={true}/>
+      {currentItems && currentItems.map((item) => (
+          <div className="w-[32%]">
+            <Product src={P1} badge={true} />
           </div>
         ))}
     </>
   );
 }
 
-
-const Pagination = ({itemsPerPage}) => {
-  // Here we use item offsets; we could also use page offsets
-  // following the API or data you're working with.
+const Pagination = ({ itemsPerPage }) => {
   const [itemOffset, setItemOffset] = useState(0);
-
-  // Simulate fetching items from another resources.
-  // (This could be items from props; or items loaded in a local state
-  // from an API endpoint with useEffect and useState)
   const endOffset = itemOffset + itemsPerPage;
-  console.log(`Loading items from ${itemOffset} to ${endOffset}`);
   const currentItems = items.slice(itemOffset, endOffset);
   const pageCount = Math.ceil(items.length / itemsPerPage);
 
-  // Invoke when user click to request another page.
   const handlePageClick = (event) => {
     const newOffset = (event.selected * itemsPerPage) % items.length;
-    console.log(
-      `User requested page number ${event.selected}, which is offset ${newOffset}`
-    );
     setItemOffset(newOffset);
   };
 
   return (
     <>
-      <div className='flex flex-wrap justify-between'>
-      <Items currentItems={currentItems} />
+      <div className="flex flex-wrap justify-between">
+        <Items currentItems={currentItems} />
       </div>
       <ReactPaginate
         breakLabel="..."
+        breakClassName="page-item"
+        breakLinkClassName="page-link"
         nextLabel=""
         onPageChange={handlePageClick}
-        pageRangeDisplayed={5}
+        pageRangeDisplayed={4}
+        marginPagesDisplayed={1}
         pageCount={pageCount}
         previousLabel=""
         renderOnZeroPageCount={null}
-        previousClassName="page-item"
-        previousLinkClassName="page-link"
-        nextClassName="page-item"
-        nextLinkClassName="page-link"
+        previousClassName="hidden"
+        nextClassName="hidden"
         pageClassName="inline-block border border-solid border-[#F0F0F0] py-2 px-3.5 font-dm font-normal text-sm"
-        // pageLinkClassName="inline-block border border-solid border-[#F0F0F0] py-2 px-3.5 font-dm font-normal text-sm text-[#] text-[#767676]"
-        containerClassName="flex gap-x-3.5"
+        containerClassName="flex gap-x-3.5 mt-12"
         activeClassName="inline-block border border-solid border-[#F0F0F0] py-2 px-3.5 font-dm font-normal text-sm text-white bg-black"
       />
+      <p className="absolute bottom-0 right-0 font-dm text-sm font-normal text-[#767676]">
+        Products from {itemOffset} to {itemOffset + itemsPerPage} of {endOffset}
+      </p>
     </>
   );
-}
+};
 
-export default Pagination
+export default Pagination;
--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,24 +1,23 @@
-import React from 'react'
-import Container from '../components/layout/Container'
-import Breadcrumb from '../components/layout/Breadcrumb'
-import Flex from '../components/layout/Flex'
-import Pagination from '../components/layout/Pagination'
+import React from "react";
+import Container from "../components/layout/Container";
+import Breadcrumb from "../components/layout/Breadcrumb";
+import Flex from "../components/layout/Flex";
+import Pagination from "../components/layout/Pagination";
 
 const Shop = () => {
-
   return (
     <>
-    <Container>
-       <Breadcrumb />
-       <Flex className="flex gap-x-10">
-          <div className='w-[30%] bg-red-600'>adad</div>
-          <div className='w-[70%]'>
-          <Pagination itemsPerPage={12} />
+      <Container>
+        <Breadcrumb />
+        <Flex className="flex gap-x-10">
+          <div className="w-[25%] bg-red-600">adad</div>
+          <div className="relative w-[75%]">
+            <Pagination itemsPerPage={12} />
           </div>
-       </Flex>
-    </Container>
+        </Flex>
+      </Container>
     </>
-  )
-}
+  );
+};
 
-export default Shop
+export default Shop;
