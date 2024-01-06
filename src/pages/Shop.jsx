e19a9202a6fa3994c4cec5c8f2e245be5dfa25a4
dev-abdullah018 <144674090+dev-abdullah018@users.noreply.github.com>
2024-01-06 16:37:40 +0600

    update-product-page-select-dropdown

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,10 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import Container from "../components/layout/Container";
 import Breadcrumb from "../components/layout/Breadcrumb";
 import Flex from "../components/layout/Flex";
 import Pagination from "../components/layout/Pagination";
 
 const Shop = () => {
+  let [showNumber,setShowNumber] = useState(12);
   return (
     <>
       <Container>
@@ -12,7 +13,34 @@ const Shop = () => {
         <Flex className="flex gap-x-10">
           <div className="w-[25%] bg-red-600">adad</div>
           <div className="relative w-[75%]">
-            <Pagination itemsPerPage={12} />
+            <div className="flex gap-x-12">
+            <div className="md:flex items-center gap-x-3.5">
+            <span className="inline-block font-dm font-normal text-base text-[#767676]">Sort by:</span>
+            <select
+              id="countries"
+              class="block md:w-[239px] w-full rounded-lg border border-[#f0f0f0] p-2.5 font-dm text-base font-normal text-[#767676] focus:border-black focus:ring-black"
+            >
+              <option selected>Featured</option>
+              <option value="US">United States</option>
+              <option value="CA">Canada</option>
+              <option value="FR">France</option>
+              <option value="DE">Germany</option>
+            </select>
+            </div>
+            <div className="md:flex items-center gap-x-3.5">
+            <span className="inline-block font-dm font-normal text-base text-[#767676]">Show:</span>
+            <select
+              onChange={(e)=> setShowNumber(e.target.value)}
+              id="countries"
+              class="block md:w-[139px] w-full rounded-lg border border-[#f0f0f0] p-2.5 font-dm text-base font-normal text-[#767676] focus:border-black focus:ring-black"
+            >
+              <option value="12">12</option>
+              <option value="24">24</option>
+              <option value="48">48</option>
+            </select>
+            </div>
+            </div>
+            <Pagination itemsPerPage={showNumber} />
           </div>
         </Flex>
       </Container>
